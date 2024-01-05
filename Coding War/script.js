3339c907a9681b2859af8877b3b0bd57692be4ad
priyankgit11 <priyankdave@weybee.in>
2024-01-05 19:39:48 +0530

    Restarted Coding War

---
+++ Coding War/script.js
@@ -0,0 +1,10 @@
+// DECLARING GLOBAL VARIABLES
+
+// SELECTING ELEMENTS
+const cardDivs = document.querySelectorAll(".div-card");
+const prevBtn = document.getElementById("btn-prev");
+const nextBtn = document.getElementById("btn-next");
+
+// ADDING EVENT LISTENERS
+
+// FUNCTIONS
